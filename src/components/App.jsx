a8edda58d8fa7d36da9c6c3fd8538083ec4cd725
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-03-16 14:55:11 +0100

    Refactored code
    Add Persistor and so on for added data to localStorage and when the page refresh put the info from localStorage to the page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter } from 'react-router-dom';
 
-import { store } from 'redux/store';
+import { store, persistor } from 'redux/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import Navbar from './Navbar/Navbar';
 import UserRoutes from 'services/userRoutes';
@@ -11,11 +12,13 @@ import UserRoutes from 'services/userRoutes';
 export const App = () => {
   return (
     <Provider store={store}>
-      <BrowserRouter basename='goit-react-hw-08-phonebook'>
-        <Navbar />
-        <UserRoutes />
-        {/* <ContactsPage /> */}
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="goit-react-hw-08-phonebook">
+          <Navbar />
+          <UserRoutes />
+          {/* <ContactsPage /> */}
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   );
 };
